538482bed2e1359583873d43ac4b83a674dcf17a
abuttle2 <alexbuttle@hotmail.co.uk>
2023-01-17 00:59:04 +0000

    Added choices for each question and click event listener for buttons

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,40 +4,57 @@ let questionsEl = document.getElementById("questions");
 let questionsTitleEl = document.getElementById("question-title");
 let endScreenEl = document.getElementById("end-screen");
 let feedbackEl = document.getElementById("feedback");
-
-var currentQuestion = 0;
+let choicesEl = document.getElementById("choices");
 
 //Monitor click events for the start button
-var startQuiz = function () {
+let startQuiz = function () {
     //Remove start screen onClick and unhide the questions
     startScreen.remove();
     questionsEl.setAttribute("class", "show");
     displayQuestions();
 }
 
-var displayQuestions = function () {
 
+var currentQuestion = 0;
+
+let displayQuestions = function () {
     // Loop through the questions and dynamically update text based on an index variable.
-    for (var i = 0; i < questions.length; i++) {
+    for (let i = 0; i < questions.length; i++) {
         if (i === currentQuestion) {
             questionsTitleEl.textContent = questions[i].question;
-            //TODO: Loop through the answers any dynamically create the options based on the answer array length.
+
             for (let j = 0; j < questions[i].choices.length; j++) {
-                console.log(questions[i].choices[j]);
+                let btnEl = document.createElement("button");
+                btnEl.textContent = j + 1 + ". " + questions[i].choices[j];
+                choicesEl.appendChild(btnEl);
+
+                btnEl.addEventListener("click", function (event) {
+                    event.target.style.color = "salmon";
+                    currentQuestion++;
+                    displayQuestions();
+                });
             }
         }
     }
-
-    switch (currentQuestion) {
-        case 0:
-            break;
-        case 1:
-            break;
-        case 2:
-            break;
-        case 3:
-            break;
-    }
 }
 
+
+
+
+// switch (currentQuestion) {
+//     case 0:
+//         break;
+//     case 1:
+//         break;
+//     case 2:
+//         break;
+//     case 3:
+//         break;
+
 startBtn.addEventListener("click", startQuiz);
+
+// var buttons = document.getElementById('button');
+
+// addEventListener("click", function () {
+
+// })
